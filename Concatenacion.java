a374367359129a890e34e423aaca96492ee5797d
ruben.martinez.castaneda <ruben.martinez.castaneda@bbva.com>
2019-05-21 17:58:15 -0500

    Creamos modulo de ejemplo de uso de array

---
+++ Concatenacion.java
@@ -0,0 +1,21 @@
+  /* Ejercicio Programa 1 –*/
+
+ 
+   public class Programa1 {
+
+ 
+         public static void main(String arg[ ]) {
+ 
+int[ ] numerodecoches = new int[24];
+
+ int r;
+
+ r = 2;
+
+ numerodecoches[r] = 23;
+
+System.out.print("El número de coches en la hora "+r+" fue "+ numerodecoches[r]);
+
+         }
+ 
+   }
